f468b603734171119041b9b6142b3233d06d79fc
Maksym <legonner@gmail.com>
2023-06-26 16:17:59 +0300

    apdate upcoming events

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,7 +1,7 @@
 const readMoreLinks = document.getElementsByClassName('blog__post-link');
 
 for (let i = 0; i < readMoreLinks.length; i++) {
-  readMoreLinks[i].addEventListener('click', function(event) {
+  readMoreLinks[i].addEventListener('click', function (event) {
     event.preventDefault();
 
     const buttonContainer = this.parentNode;
@@ -20,11 +20,22 @@ for (let i = 0; i < readMoreLinks.length; i++) {
 }
 
 const countdownElement = document.getElementById('countdown');
-const endDate = new Date('June 30, 2023');
+
+let currentSlideIndex = 0;
+const slideData = [
+  {
+    endDate: new Date('June 30, 2023'),
+    imageSrc: 'images/three-people-drink-beer.jpeg',
+  },
+  {
+    endDate: new Date('July 15, 2023'),
+    imageSrc: 'images/three-people-drink-beer.jpeg',
+  },
+];
 
 function updateCountdown() {
   const currentDate = new Date();
-  const remainingTime = endDate - currentDate;
+  const remainingTime = slideData[currentSlideIndex].endDate - currentDate;
 
   if (remainingTime < 0) {
     countdownElement.textContent = 'Time is up!';
@@ -59,5 +70,88 @@ function updateCountdown() {
   `;
 }
 
-updateCountdown();
+function updateSlide(index) {
+  const slide = slideData[index];
+  const countdownImage = document.getElementById('blog__countdown-image');
+  const countdownDescription = document.querySelector('.blog__description-count');
+
+  countdownImage.src = slide.imageSrc;
+  countdownDescription.textContent = slide.description;
+  updateCountdown();
+}
+
+function handlePrevSlide() {
+  currentSlideIndex = (currentSlideIndex - 1 + slideData.length) % slideData.length;
+  updateSlide(currentSlideIndex);
+}
+
+function handleNextSlide() {
+  currentSlideIndex = (currentSlideIndex + 1) % slideData.length;
+  updateSlide(currentSlideIndex);
+}
+
+updateSlide(currentSlideIndex);
 setInterval(updateCountdown, 1000);
+
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+
+prevBtn.addEventListener('click', handlePrevSlide);
+nextBtn.addEventListener('click', handleNextSlide);
+
+// Slider
+
+const slides = [
+  {
+    image: 'images/three-people-drink-beer.jpeg',
+    title: 'BAVARIAN BEER DAY',
+    description: 'this Friday',
+    imageCssProperties: {
+      width: '100%',
+    },
+  },
+  {
+    image: 'images/ukrainian-festival.png',
+    title: 'UKRAINIAN BEER FESTIVAL',
+    description: 'next Saturday',
+    imageCssProperties: {
+      width: '70%',
+      margin: 'auto',
+    },
+  },
+];
+
+let slideIndex = 0;
+const countdownImage = document.getElementById('blog__countdown-image');
+const sliderTitle = document.getElementById('sliderTitle');
+const slideDescription = document.getElementById('slideDescription');
+
+function showSlide(index) {
+  countdownImage.src = slides[index].image;
+
+  Object.assign(countdownImage.style, slides[index].imageCssProperties);
+
+  sliderTitle.textContent = slides[index].title;
+  slideDescription.textContent = slides[index].description;
+}
+
+function nextSlide() {
+  slideIndex++;
+  if (slideIndex >= slides.length) {
+    slideIndex = 0;
+  }
+  showSlide(slideIndex);
+}
+
+function prevSlide() {
+  slideIndex--;
+  if (slideIndex < 0) {
+    slideIndex = slides.length - 1;
+  }
+  showSlide(slideIndex);
+}
+
+prevBtn.addEventListener('click', prevSlide);
+nextBtn.addEventListener('click', nextSlide);
+
+showSlide(slideIndex);
