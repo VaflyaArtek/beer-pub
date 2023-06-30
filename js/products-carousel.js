1ec722465e2c8e14c05a77dbfbe47b062c9951eb
Marina <135515516+Sviezhynska@users.noreply.github.com>
2023-06-30 07:18:00 +0000

    fix products-carousel

--- js/products-carousel.js
+++ js/products-carousel.js
@@ -2,7 +2,7 @@ const carousel = document.querySelector('.products__carousel');
 const carouselInner = carousel.querySelector('.products__carousel-container');
 const prevButton = carousel.querySelector('.products__carousel--btn-prev');
 const nextButton = carousel.querySelector('.products__carousel--btn-next');
-const productCards = carouselInner.querySelectorAll('.products__carousel-slide');
+let productCards = carouselInner.querySelectorAll('.products__carousel-slide');
 
 let slidesPerView = 1;
 let currentIndex = 0;
@@ -24,15 +24,24 @@ function updateCarousel() {
     carouselInner.style.transform = `translateX(-${currentIndex * (100 / slidesPerView)}%)`;
 }
 
-prevButton.addEventListener('click', () => {
+cloneSlides();
+
+function cloneSlides() {
+  const firstSlideClone = productCards[0].cloneNode(true);
+  const lastSlideClone = productCards[productCards.length - 1].cloneNode(true);
+
+  carouselInner.appendChild(firstSlideClone);
+  carouselInner.insertBefore(lastSlideClone, productCards[0]);
+
+  productCards = carouselInner.querySelectorAll('.products__carousel-slide'); 
+}
+
+prevButton.addEventListener('click', function() {
     currentIndex = Math.max(currentIndex - 1, 0);
     updateCarousel();
 });
 
-nextButton.addEventListener('click', () => {
+nextButton.addEventListener('click', function() {
     currentIndex = Math.min(currentIndex + 1, productCards.length - slidesPerView);
     updateCarousel();
 });
-
-
-
