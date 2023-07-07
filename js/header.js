da512a49b9ebb62dbdca4c263ca00a78fa2c75cc
Perkhun <90705936+Perkhun@users.noreply.github.com>
2023-07-07 19:38:41 +0000

    change styles

--- js/header.js
+++ js/header.js
@@ -1,17 +1,19 @@
-const words = document.querySelectorAll('.nav-link');
+var words = document.querySelectorAll('.underline-on-hover');
 
-words.forEach(function (word) {
-    word.addEventListener("mouseenter", addUnderline);
-    word.addEventListener("mouseleave", removeUnderline);
+words.forEach(function(word) {
+  word.addEventListener("mouseenter", function() {
+    this.style.textDecoration = "underline";
+    this.style.transitionDuration = "5s";
+  });
+
+  word.addEventListener("mouseleave", function() {
+    var self = this;
+    setTimeout(function() {
+      self.style.textDecoration = "none";
+    }, 5000);
+  });
 });
 
-function addUnderline() {
-    this.style.textDecoration = "underline";
-}
-
-function removeUnderline() {
-    this.style.textDecoration = "none";
-}
 
 
 
