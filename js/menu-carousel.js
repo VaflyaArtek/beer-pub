5fe66582264aeb894b1d94de95f93ac8a047be95
Natalia Silina <natikkr@gmail.com>
2023-07-10 15:15:26 +0300

    fix menu-carousel

--- js/menu-carousel.js
+++ js/menu-carousel.js
@@ -5,71 +5,65 @@ let count = 0;
 let width;
 
 function init() {
-    console.log('resize');
-    width = document.querySelector('.carousel__slide-container').offsetWidth;
+  console.log('resize');
+  width = document.querySelector('.carousel__slide-container').offsetWidth;
+  const screenWidth = window.innerWidth;
 
-    if (window.innerWidth < 720) {
-        barItem.forEach(item => {
-            item.style.width = width + 'px';
-            item.style.height = 'auto';
-        });
+  if (screenWidth < 720) {
+    barItem.forEach(item => {
+      item.style.width = width + 'px';
+      item.style.height = 'auto';
+    });
 
-        rollCarouselFull();
-    } else {
-        barItem.forEach(item => {
-            item.style.width = width / 2 + 'px';
-            item.style.height = 'auto';
-        });
+    rollCarousel(screenWidth);
+  } else {
+    barItem.forEach(item => {
+      item.style.width = width / 2 + 'px';
+      item.style.height = 'auto';
+    });
 
-        carouselItem.forEach(item => {
-            item.style.width = width * barItem.length + 'px';
-        });
+    carouselItem.forEach(item => {
+      item.style.width = width * barItem.length + 'px';
+    });
 
-        rollCarousel();
-    }
+    rollCarousel(screenWidth);
+  }
 
-    console.log(width);
+  console.log(width);
 }
 
 window.addEventListener('resize', init);
 init();
 
 document.querySelector('.bar__btn-prev').addEventListener('click', function () {
-    count--;
-    if (count < 0) {
-        count = carouselItem.length - 1;
-    }
+  count--;
+  if (count < 0) {
+    count = carouselItem.length - 1;
+  }
 
-    if (window.innerWidth < 720) {
-        rollCarouselFull();
-    } else {
-        rollCarousel();
-    }
+  const screenWidth = window.innerWidth;
+  rollCarousel(screenWidth);
 });
 
 document.querySelector('.bar__btn-next').addEventListener('click', function () {
-    count++;
-    if (count >= carouselItem.length) {
-        count = 0;
-    }
-    
+  count++;
+  if (count >= carouselItem.length) {
+    count = 0;
+  }
 
-    if (window.innerWidth < 720) {
-        rollCarouselFull();
-    } else {
-        rollCarousel();
-    }
+  const screenWidth = window.innerWidth;
+  rollCarousel(screenWidth);
 });
 
-function rollCarousel() {
+function rollCarousel(screenWidth) {
+  if (screenWidth < 720) {
     carouselItem.forEach(item => {
-        item.style.transform = 'translateX(-' + count * width / 2 + 'px)';
+      item.style.transform = 'translateX(-' + count * width + 'px)';
     });
-}
-
-function rollCarouselFull() {
+  } else {
     carouselItem.forEach(item => {
-        item.style.transform = 'translateX(-' + count * width + 'px)';
+      item.style.transform = 'translateX(-' + count * width / 2 + 'px)';
     });
+  }
 }
 
